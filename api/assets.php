0ff8594e780f69a56403463734043918a8562511
Altair <arkantaqiddin09@gmail.com>
2025-01-24 07:41:41 +0700

    Update nambah foto login assets.php

--- api/assets.php
+++ api/assets.php
@@ -4,4 +4,22 @@ if ($_GET['type'] === 'css') {
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
     echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+} else if ($_GET['type'] === 'image') {
+    $filePath = __DIR__ . '/../public/assets/' . basename($_GET['file']);
+    $fileInfo = pathinfo($filePath);
+    $mimeTypes = [
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'png' => 'image/png',
+        'gif' => 'image/gif',
+        'svg' => 'image/svg+xml',
+    ];
+
+    if (file_exists($filePath) && isset($mimeTypes[$fileInfo['extension']])) {
+        header('Content-Type: ' . $mimeTypes[$fileInfo['extension']]);
+        readfile($filePath);
+    } else {
+        http_response_code(404);
+        echo "File not found.";
+    }
 }
